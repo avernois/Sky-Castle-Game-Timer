911e140ea842e4f4be930b3b307ddd5af6a9fdc8
Antoine <antoine@crafting-labs.fr>
2011-11-06 00:12:44 +0100

    refactoring

--- scg-time.js
+++ scg-time.js
@@ -1,37 +1,45 @@
 time = 3000;
 var paper;
 
-function moveCircle(circles, i) {
-    var circle = circles[i];
+
+function playSong() {
     var song = document.getElementById('audio');
     song.play(); 
+};
+
+function moveCircle(circles, i) {
+    var circle = circles[i];
+    circle.cx = 300 + i*30;
+    circle.animate({cx: circle.cx, fill: circle.cx - 100 ? "hsb(.3, .75, .75)" : "#000", "fill-opacity": +!!(circle.cx - 100)}, time, "linear", 
+      function() {
+          moveCircles(circles, i - 1);
+      });
+};
+
+function moveCircles(circles, i) {
+    var circle = circles[i];
+   
+    playSong();
+   
     if(i == 10/2 - 1) {
-        t = paper.text(100, 100, "Vous avez de la chance ?");
+        t = paper.text(300, 100, "Vous avez de la chance ?");
         t.attr("font-size", 40);
         t.click(
             function() {
-            circle.cx = 300 + i*30;
-            circle.animate({cx: circle.cx, fill: circle.cx - 100 ? "hsb(.3, .75, .75)" : "#000", "fill-opacity": +!!(circle.cx - 100)}, time, "linear", 
-                function() {
-                    moveCircle(circles, i - 1);
-                });
-            t.remove();
+                moveCircle(circles, i);
+                t.remove();
             }
         );
         
     } else {
         if ( i >= 0) {
-            circle.cx = 300 + i*30;
-            circle.animate({cx: circle.cx, fill: circle.cx - 100 ? "hsb(.3, .75, .75)" : "#000", "fill-opacity": +!!(circle.cx - 100)}, time, "linear", 
-                function() {
-                    moveCircle(circles, i - 1);
-                });
+	    moveCircle(circles, i);
         }
     }
 };
 
 function initForm () {
-     paper = Raphael("sample-1", 800, 600);
+     paper = Raphael("scg", 800, 600);
 
     var circles = paper.set();
    
@@ -40,6 +48,6 @@ function initForm () {
     }
 
     circles.attr({fill: "#000", stroke: "#fff", "fill-opacity": 100});
-    moveCircle(circles, 9);
+    moveCircles(circles, 9);
 };
 
