05201c970a510a7d8e80ff85e41ea80195cfee6f
Antoine <antoine@crafting-labs.fr>
2011-11-06 13:28:28 +0100

    ajout d'un son de gong pour le jour de chance et de rétrospective demarrage sur un click

--- scg-time.js
+++ scg-time.js
@@ -1,10 +1,15 @@
-time = 2000;
+time = 20000;
 nb_iterations = 5;
 var paper;
 
 
 function playSong() {
     var song = document.getElementById('audio');
+    song.play(); 
+};
+
+function playGong() {
+    var song = document.getElementById('gong');
     song.play(); 
 };
 
@@ -19,34 +24,39 @@ function moveCircle(iterations, i, j) {
 };
 
 function moveCircles(iterations, i, j) {
-    playSong();
-   
+
     if(i == 10/2 - 1) {
+    	playGong();
         t = paper.text(400, 500, "Vous avez de la chance ?");
         t.attr("font-size", 40);
         t.click(
             function() {
+    		playGong();
                 moveCircle(iterations, i, j);
                 t.remove();
             }
         );
     } else  {
         if ( i < 0) { // fin d'itération
-          playSong();
-          t = paper.text(400, 500, "Rétrospective !");
-          t.attr("font-size", 40);
-          t.click(
-            function() {
+            playGong();
+            t = paper.text(400, 500, "Rétrospective !");
+            t.attr("font-size", 40);
+            t.click(
+              function() {
                 t.remove();
                 if (j < nb_iterations - 1) {
+                    playGong();
 	            moveCircle(iterations, 9, j + 1);
                 }
-            }
-          );
+              }
+            );
         } else {
-            if ( i >= 0) {
-	        moveCircle(iterations, i, j);
+            if (i == 9) { // début d'itération
+                playGong();
+            } else {
+                playSong();
             }
+	    moveCircle(iterations, i, j);
         }
     }
 };
@@ -56,18 +66,25 @@ function initForm () {
     paper.rect(790, 590, 800, 600);
 
     var iterations = paper.set();
+
   
     for (j=0; j < nb_iterations; j++) {
         var circles = paper.set();
+        paper.rect(50, 98 + j * 60, 740, 4); 
+
    
         for (i = 0; i < 10; i++) {  
             circles.push(paper.circle(50 + 30 * i, 100 + j*60, 25));
         }
-
         circles.attr({fill: "#000", stroke: "#fff", "fill-opacity": 100});
         iterations.push(circles); 
     }
-    
-    moveCircles(iterations, 9, 0);
+   
+    var t = paper.text(400, 500, "Démarrer").click(
+      function() {
+          moveCircles(iterations, 9, 0);
+          t.remove();
+      }
+    );
 };
 
