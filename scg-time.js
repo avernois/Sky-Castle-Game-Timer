397bf038cf853674517c976cf312556f85617e66
Antoine <antoine@crafting-labs.fr>
2011-11-06 15:21:30 +0100

    affichage de marqueur de temps pour les jours

--- scg-time.js
+++ scg-time.js
@@ -1,5 +1,6 @@
-time = 20000;
+time = 1000;
 nb_iterations = 5;
+nb_days = 10; 
 var paper;
 
 
@@ -14,8 +15,19 @@ function playGong() {
 };
 
 function moveCircle(iterations, i, j) {
-    var circles = iterations[j];
+    var iteration = iterations[j];
+    var circles = iteration[0];
     var circle = circles[i];
+
+
+    if (i < nb_days - 1) {
+       var prevMark = iterations[j][1][i + 1];
+       prevMark.hide();
+    } ;
+    
+    var mark = iterations[j][1][i];
+    mark.show();
+
     circle.cx =  500 + i*30;
     circle.animate({cx: circle.cx, fill: circle.cx - 100 ? "hsb(.3, .75, .75)" : "#000", "fill-opacity": +!!(circle.cx - 100)}, time, "linear", 
       function() {
@@ -25,7 +37,7 @@ function moveCircle(iterations, i, j) {
 
 function moveCircles(iterations, i, j) {
 
-    if(i == 10/2 - 1) {
+    if(i == (nb_days/2 - 1)) {
     	playGong();
         t = paper.text(400, 500, "Vous avez de la chance ?");
         t.attr("font-size", 40);
@@ -46,12 +58,12 @@ function moveCircles(iterations, i, j) {
                 t.remove();
                 if (j < nb_iterations - 1) {
                     playGong();
-	            moveCircle(iterations, 9, j + 1);
+	            moveCircle(iterations, nb_days - 1, j + 1);
                 }
               }
             );
         } else {
-            if (i == 9) { // début d'itération
+            if (i == (nb_days - 1)) { // début d'itération
                 playGong();
             } else {
                 playSong();
@@ -61,28 +73,44 @@ function moveCircles(iterations, i, j) {
     }
 };
 
+function timeMark(day, iteration) {
+    var marks = paper.set(); 
+    marks.push(paper.rect(124 + day * 30, 95 + iteration * 60, 2 , 10).hide());
+    marks.push(paper.rect(249 + day * 30, 93 + iteration * 60, 2 , 14).hide());
+    marks.push(paper.rect(374 + day * 30, 95 + iteration * 60, 2 , 10).hide());
+    return marks;
+};
+
+
 function initForm () {
     paper = Raphael("scg", 800, 600);
-    paper.rect(790, 590, 800, 600);
+    paper.rect(790, 590, 10, 10);
 
     var iterations = paper.set();
-
+    
   
     for (j=0; j < nb_iterations; j++) {
-        var circles = paper.set();
-        paper.rect(50, 98 + j * 60, 740, 4); 
 
+        var iteration = paper.set();
+        var circles = paper.set();
+        var marks = paper.set();
+        paper.rect(50, 98 + j * 60, 740, 4); 
    
         for (i = 0; i < 10; i++) {  
             circles.push(paper.circle(50 + 30 * i, 100 + j*60, 25));
+            marks.push(timeMark(i, j));
         }
         circles.attr({fill: "#000", stroke: "#fff", "fill-opacity": 100});
-        iterations.push(circles); 
+  
+        iteration.push(circles);
+        iteration.push(marks);
+ 
+        iterations.push(iteration); 
     }
-   
+
     var t = paper.text(400, 500, "Démarrer").click(
       function() {
-          moveCircles(iterations, 9, 0);
+          moveCircles(iterations, nb_days - 1, 0);
           t.remove();
       }
     );
