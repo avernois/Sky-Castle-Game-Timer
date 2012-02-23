f2192c95401d2d36164aebf674fb6899353e01c6
Antoine <antoine.vernois@gmail.com>
2012-02-23 02:53:15 +0100

    Add a button to switch between manual/auto mode. Improve readibility of the code.

--- js/scg-time.js
+++ js/scg-time.js
@@ -1,15 +1,61 @@
-DAY_DURATION = 500;
-RETRO_DURATION = 1000;
+DAY_DURATION = 30000;
+RETRO_DURATION = 300000;
 NB_ITERATIONS = 5;
 NB_DAYS = 10; 
-var paper;
-var sound = false;
 
-var manual = false;
+DEFAULT_SOUND_STATE = false;
+DEFAULT_MANUAL_STATE = true;
+
+
+
+var sound = DEFAULT_SOUND_STATE;
+var manual = DEFAULT_MANUAL_STATE;
+
+var paper;
+//
+// main function of that script.
+// It aims to initialize everything needed.
+// It then starts the bord in order to play Sky Castle Game
+//
+
+function launchSCGBoard () {
+    paper = Raphael("scg", 800, 600);
+
+    var iterations = paper.set();
+    
+    for (current_iteration=0; current_iteration < NB_ITERATIONS; current_iteration++) {
+
+        var iteration = paper.set();
+        var circles = paper.set();
+        var marks = paper.set();
+        paper.rect(50, 98 + current_iteration * 60, 720, 4); 
+   
+        for (day = 0; day < NB_DAYS; day++) {  
+            circles.push(paper.circle(50 + 30 * day, 100 + current_iteration*60, 25));
+            marks.push(timeMark(day, current_iteration));
+        }
+        circles.attr({fill: "#000", stroke: "#fff", "fill-opacity": 100});
+  
+        iteration.push(circles);
+        iteration.push(marks);
+ 
+        iterations.push(iteration); 
+    }
+
+    nextDay(iterations, NB_DAYS - 1, 0);
+
+    soundMgmt();
+    manualMgmt();
+};
+
+
+//
+//
+//
 
 function nextDay(iterations, day_left, current_iteration) {
 
-    if (manual || day_left == (NB_DAYS -1)) {
+    if (manual || isFirstDay(day_left)) {
         t = text("Démarrer le jour " + (NB_DAYS - day_left));
 
         t.click(
@@ -42,7 +88,7 @@ function moveCircle(iterations, day_left, current_iteration) {
 
 function moveCircles(iterations, day_left, current_iteration) {
 
-    if(day_left == (NB_DAYS/2 - 1)) {
+    if(isMidWeek(day_left)) {
     	playGong();
         t = text("Vous avez de la chance ?");
         t.click(
@@ -53,11 +99,11 @@ function moveCircles(iterations, day_left, current_iteration) {
             }
         );
     } else  {
-        if ( day_left < 0) { // fin d'itération
+        if (isIterationOver(day_left)) { // fin d'itération
             playGong();
             retroTime(iterations, current_iteration);
         } else {
-            if (day_left == (NB_DAYS - 1)) { // début d'itération
+            if (isFirstDay(day_left)) { // début d'itération
                 playGong();
             } else {
                 playSong();
@@ -104,35 +150,6 @@ function retroTime(iterations, current_iteration) {
 
 
 
-function initForm () {
-    paper = Raphael("scg", 800, 600);
-
-    var iterations = paper.set();
-    
-  
-    for (current_iteration=0; current_iteration < NB_ITERATIONS; current_iteration++) {
-
-        var iteration = paper.set();
-        var circles = paper.set();
-        var marks = paper.set();
-        paper.rect(50, 98 + current_iteration * 60, 720, 4); 
-   
-        for (day = 0; day < NB_DAYS; day++) {  
-            circles.push(paper.circle(50 + 30 * day, 100 + current_iteration*60, 25));
-            marks.push(timeMark(day, current_iteration));
-        }
-        circles.attr({fill: "#000", stroke: "#fff", "fill-opacity": 100});
-  
-        iteration.push(circles);
-        iteration.push(marks);
- 
-        iterations.push(iteration); 
-    }
-
-    nextDay(iterations, NB_DAYS - 1, 0);
-
-    soundMgmt();
-};
 
 
 // Time mark
@@ -174,49 +191,82 @@ function playGong() {
     }
 };
 
-function soundOnButton() {
-    return paper.path(soundOnPath).attr({fill: "#000", stroke: "none"});
-}
-
-function soundOffButton() {
-    return paper.path(soundOffPath).attr({fill: "#000", stroke: "none"});
+function soundButton(path) {
+    return paper.path(path).attr({fill: "#000", stroke: "none"}).hide().translate(770, 575);
 }
 
 function soundMgmt() {
-    var soundOn = soundOnButton().hide();
-    var soundOff = soundOffButton().hide();
+    var soundOn = soundButton(soundOnPath);
+    var soundOff = soundButton(soundOffPath);
 
-    soundOn.translate(770, 575).click(
+    buttonMgmt(soundOn, soundOff,
       function () {
-          soundOn.hide();
-          soundOff.show();
-          sound = false;
-      }
-    );
-    
-    soundOff.translate(770, 575).click(
-      function () {
-          soundOff.hide();
-          soundOn.show();
-          sound = true;
+          sound = !sound;
       }
     );
 
-    if (sound) {
-        soundOn.show();
-    } else {
-        soundOff.show();
-    }
+    showButton(sound, soundOn, soundOff);
 }
-
-
 
 var soundOnPath = "M4.998,12.127v7.896h4.495l6.729,5.526l0.004-18.948l-6.73,5.526H4.998z M18.806,11.219c-0.393-0.389-1.024-0.389-1.415,0.002c-0.39,0.391-0.39,1.024,0.002,1.416v-0.002c0.863,0.864,1.395,2.049,1.395,3.366c0,1.316-0.531,2.497-1.393,3.361c-0.394,0.389-0.394,1.022-0.002,1.415c0.195,0.195,0.451,0.293,0.707,0.293c0.257,0,0.513-0.098,0.708-0.293c1.222-1.22,1.98-2.915,1.979-4.776C20.788,14.136,20.027,12.439,18.806,11.219z M21.101,8.925c-0.393-0.391-1.024-0.391-1.413,0c-0.392,0.391-0.392,1.025,0,1.414c1.45,1.451,2.344,3.447,2.344,5.661c0,2.212-0.894,4.207-2.342,5.659c-0.392,0.39-0.392,1.023,0,1.414c0.195,0.195,0.451,0.293,0.708,0.293c0.256,0,0.512-0.098,0.707-0.293c1.808-1.809,2.929-4.315,2.927-7.073C24.033,13.24,22.912,10.732,21.101,8.925z M23.28,6.746c-0.393-0.391-1.025-0.389-1.414,0.002c-0.391,0.389-0.391,1.023,0.002,1.413h-0.002c2.009,2.009,3.248,4.773,3.248,7.839c0,3.063-1.239,5.828-3.246,7.838c-0.391,0.39-0.391,1.023,0.002,1.415c0.194,0.194,0.45,0.291,0.706,0.291s0.513-0.098,0.708-0.293c2.363-2.366,3.831-5.643,3.829-9.251C27.115,12.389,25.647,9.111,23.28,6.746z";
 var soundOffPath = "M4.998,12.127v7.896h4.495l6.729,5.526l0.004-18.948l-6.73,5.526H4.998z";
 
+//
+// Create & management manual/auto button
+//
+function textButton(text) {
+  return paper.text(0, 0, text).attr("font-size", 12).translate(750, 590).hide();
+}
 
+function manualMgmt() {
+    var manualButton = textButton("manual");
+    var autoButton = textButton("auto");
+
+
+    buttonMgmt(manualButton, autoButton, 
+      function () {
+          manual = !manual;
+      }
+    );
+
+    showButton(manual, manualButton, autoButton);
+}
+
+//
+// button management
+//
+function showButton(enable, enabledButton, disabledButton) {
+  if (enable) {
+    enabledButton.show();
+    disabledButton.hide();
+  } else {
+    enabledButton.hide();
+    disabledButton.show();
+  }
+}
+
+
+function buttonMgmt(enabledButton, disabledButton, changeStateCallback) {
+  enabledButton.click(
+      function () {
+          enabledButton.hide();
+          disabledButton.show();
+          changeStateCallback();
+      }
+    );
+    
+    disabledButton.click(
+      function () {
+          disabledButton.hide();
+          enabledButton.show();
+          changeStateCallback.call();
+      }
+    );  
+}
+
+//
 // Utilities
-
+//
 function text(text) {
 
     var t = paper.text(400, 500, text);
@@ -224,3 +274,15 @@ function text(text) {
 
     return t;
 };
+
+function isFirstDay(day_left) {
+  return day_left == (NB_DAYS -1);
+}
+
+function isMidWeek(day_left) {
+  return day_left == (NB_DAYS/2 - 1);
+}
+
+function isIterationOver(day_left) {
+  return day_left < 0;
+}
